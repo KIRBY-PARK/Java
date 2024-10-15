678818e8e8bf156633d6508bd9de125e073047db
커비 <pkirby0426@gmail.com>
2024-10-15 17:59:32 +0900

    Java 기초

---
+++ java-uni/HelloSwing.java
@@ -0,0 +1,14 @@
+// HelloSwing.java
+
+import javax.swing.JFrame;
+
+public class HelloSwing {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        myframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myframe.setTitle("Hello Swing!");
+        myframe.setSize(300, 300);
+        myframe.setVisible(true);
+    }
+}
