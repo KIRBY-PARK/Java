f72148788ad42321b796371cc0dfe091b2d38594
커비 <pkirby0426@gmail.com>
2024-10-21 23:27:07 +0900

    인프런 자바 강의 수강 후 실습

---
+++ inflearn/HelloKirby.java
@@ -0,0 +1,5 @@
+public class HelloKirby {
+    public static void main(String[] args) {
+        System.out.println("Hello Kirby");
+    }
+}
---
+++ inflearn/HelloKirby2.java
@@ -0,0 +1,7 @@
+public class HelloKirby2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Kirby1");
+        System.out.println("Hello Kirby2");
+        System.out.println("Hello Kirby3");
+    }
+}
