5ffe1d0361334dcdb6bc5e07cb1bf5abb02229c9
커비 <pkirby0426@gmail.com>
2024-11-01 00:27:25 +0900

    Java 반복문 학습
    - while문
    - do-while문
    - for문

---
+++ sep_1101/While1_2.java
@@ -0,0 +1,12 @@
+package sep_1101;
+
+public class While1_2 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 3) { // 3보다 작을 때까지 반복
+            count = count + 1;
+            System.out.println("현재 숫자는: " + count);
+        }
+    }
+}
